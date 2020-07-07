9f7d249a23f051b71cecbe85aa3fc05f49ded368
djalabz <djalabz@gmail.com>
2020-07-07 16:06:04 +0200

    Overlay_fixed_with_header

--- js/overlay.js
+++ js/overlay.js
@@ -1,10 +1,12 @@
 // Overlay Menu
 
+var header = document.getElementById('header');
 var overlay = document.getElementById('overlay');
 var closeMenu = document.getElementById('closeMenu');
 
 document.getElementById('header').addEventListener('click', function() {
     overlay.classList.add('show-menu');
+    header.classList.add('hide-header');
 });
 
 document.getElementById('closeMenu').addEventListener('click', function() {
