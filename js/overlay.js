20981bd5a4abd870f0087bf348b5fdaa7935d107
djalabz <djalabz@gmail.com>
2020-07-08 20:14:51 +0200

    newMenu

--- js/overlay.js
+++ js/overlay.js
@@ -3,19 +3,87 @@
 var header = document.getElementById('header');
 var overlay = document.getElementById('overlay');
 var closeMenu = document.getElementById('closeMenu');
+var workOverlay = document.getElementById('workOverlay');
+var portfolioOverlay = document.getElementById('portfolioOverlay');
+var aboutOverlay = document.getElementById('aboutOverlay');
 
-document.getElementById('header').addEventListener('click', function() {
-    overlay.classList.add('show-menu');
+// document.getElementById('header').addEventListener('click', function() {
+//     overlay.classList.add('show-menu');
+//     header.classList.add('hide-header');
+//     overlay.classList.remove('close-menu');
+//     header.classList.remove('show-header');
+// });
+
+
+// WORK SECTION
+
+
+
+document.getElementById('work').addEventListener('click', function() {
+    workOverlay.classList.add('show-work');
     header.classList.add('hide-header');
-    overlay.classList.remove('close-menu');
+    workOverlay.classList.remove('close-menu');
     header.classList.remove('show-header');
 });
 
 document.getElementById('closeMenu').addEventListener('click', function() {
-    overlay.classList.remove('show-menu');
+    workOverlay.classList.remove('show-work');
     header.classList.remove('hide-header');
     header.classList.add('show-header');
-    overlay.classList.add('close-menu');
+    workOverlay.classList.add('close-menu');
 });
 
-// WORK
+
+// PORTFOLIO SECTION
+
+
+document.getElementById('portfolio').addEventListener('click', function() {
+    portfolioOverlay.classList.add('show-portfolio');
+    header.classList.add('hide-header');
+    portfolioOverlay.classList.remove('close-menu');
+    header.classList.remove('show-header');
+});
+
+document.getElementById('closeFolio').addEventListener('click', function() {
+    portfolioOverlay.classList.remove('show-portfolio');
+    header.classList.remove('hide-header');
+    header.classList.add('show-header');
+    portfolioOverlay.classList.add('close-menu');
+});
+
+
+// ABOUT SECTION
+
+
+document.getElementById('about').addEventListener('click', function() {
+    aboutOverlay.classList.add('show-about');
+    header.classList.add('hide-header');
+    aboutOverlay.classList.remove('close-menu');
+    header.classList.remove('show-header');
+});
+
+document.getElementById('closeAbout').addEventListener('click', function() {
+    aboutOverlay.classList.remove('show-about');
+    header.classList.remove('hide-header');
+    header.classList.add('show-header');
+    aboutOverlay.classList.add('close-menu');
+});
+
+
+
+// CONTACT SECTION
+
+
+document.getElementById('contact').addEventListener('click', function() {
+    contactOverlay.classList.add('show-contact');
+    header.classList.add('hide-header');
+    contactOverlay.classList.remove('close-menu');
+    header.classList.remove('show-header');
+});
+
+document.getElementById('closeContact').addEventListener('click', function() {
+    contactOverlay.classList.remove('show-contact');
+    header.classList.remove('hide-header');
+    header.classList.add('show-header');
+    contactOverlay.classList.add('close-menu');
+});
--- js/work.js
+++
@@ -1,7 +0,0 @@
-// WORK PAGE
-
-var work = document.getElementById('work');
-
-document.getElementById('work').addEventListener('click', function() {
-    overlay.classList.add('work-page');
-});
