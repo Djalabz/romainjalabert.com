9e0b3c235ad56c133fda262c1db004edc26b6df2
djalabz <djalabz@gmail.com>
2020-07-08 17:03:00 +0200

    FadeOutMenuAnim

--- js/overlay.js
+++ js/overlay.js
@@ -7,11 +7,15 @@ var closeMenu = document.getElementById('closeMenu');
 document.getElementById('header').addEventListener('click', function() {
     overlay.classList.add('show-menu');
     header.classList.add('hide-header');
+    overlay.classList.remove('close-menu');
+    header.classList.remove('show-header');
 });
 
 document.getElementById('closeMenu').addEventListener('click', function() {
     overlay.classList.remove('show-menu');
     header.classList.remove('hide-header');
+    header.classList.add('show-header');
+    overlay.classList.add('close-menu');
 });
 
 // WORK
