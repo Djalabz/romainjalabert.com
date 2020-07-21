0757d658a2194749aad5f9867281fa5a54a6403c
djalabz <djalabz@gmail.com>
2020-07-21 21:45:11 +0200

    Resume_link_added

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,26 +1,53 @@
 
 // CONTACT FORM //
 
+$(document).ready(function () {
+$('.submit-button').click(function (event) {
+        event.preventDefault();
+        
+        var to = "romainjalabet.pro@gmail.com";
+        var fname = $("#fname").val();
+        var lname = $("#lname").val();
+        var email = $("#email").val();
+        var text = $("#message").val();
+        var status = $('.statusForm');
+        status.empty();
+
+        if(email.length>5 && email.includes('@') && email.includes('.')) {
+            status.append(<div>email valid</div>)
+        } else {
+            status.append(<div>email non-valid</div>)
+        }
+
+        if(subject.length>2)  {
+            status.append(<div>subject valid</div>)
+        } else {
+            status.append(<div>subject non-valid</div>)
+        }
+
+        if(fname.length>2 && lname.length>2)  {
+            status.append(<div>name valid</div>)
+        } else {
+            status.append(<div>name non-valid</div>)
+        }
+
+        if(text.length>2) {
+            status.append(<div>message valid</div>)
+        } else {
+            status.append(<div>message non-valid</div>)
+        }
 
 
-document.getElementById('submit').addEventListener('click', function () {
-    var user, to, subject, text;
-        // enter your email account that you want to recieve emails at.
-        to = "romainjalabet.pro@gmail.com";
-        fname = $("#fname").val();
-        lname = $("#lname").val();
-        user = $("#user_email").val();
-        text = $("#message").val();
-        // $("#message").text("Sending E-mail...Please wait");
-        $.get("http://localhost:8080/send", {
-            to: to,
-            name: name,
-            user: user,
-            text: text
-        }, function (data) {
-            if (data == "sent") {
-                console.log("Email sent");
-            }
-        });
-    });
-;
+        // $.get("http://localhost:8080/send", {
+        //     to: to,
+        //     fname: fname,
+        //     lname: lname,
+        //     email: email,
+        //     text: text
+        // }, function (data) {
+        //     if (data == "sent") {
+        //         console.log("Email sent");
+        //     }
+        // });
+    })
+})
